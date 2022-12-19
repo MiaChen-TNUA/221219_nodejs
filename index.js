5f952f48daf9e126166e83f34aff0dde8f0af08f
MiaChen-TNUA <b10962034@gmcc.tnua.edu.tw>
2022-12-19 18:35:18 +0800

    add index

--- index.js
+++ index.js
@@ -27,3 +27,24 @@ console.log(os.hostname())
 const greeting = require('./src/greeting');
 
 greeting.hello()
+
+//載入express 模組
+const express=require("express");
+
+//建立 application 物件
+const app=express();
+
+//設定路徑 / 對應的路由函式
+app.get("/",function(req, res){
+res.send("妳好啊，妳翹課哦！")
+});
+
+//設定路徑 /getData 對應的路由函式
+app.get("/getData",function(req, res){
+res.send("被我抓到了喔！")
+});
+
+//啟動伺服器在測試網址 http://localhost:3000
+app.listen(3000, function(){
+console.log("Server started");
+})
